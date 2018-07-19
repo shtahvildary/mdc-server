ffc65963af79dd73b2be68ee667e2cd4350a9ab1
shtahvildary <shtahvildary@gmail.com>
2018-07-20 00:46:46 +0430

    search api is added to netnode

--- src/app/controllers/netNodeController.js
+++ src/app/controllers/netNodeController.js
@@ -1,7 +1,7 @@
 import NetNode from '../models/NetNode'
 
 /*          POST /api/netnodes/new            */
-export let newNetNode=async(req,res)=>{
+export let new_NetNode=async(req,res)=>{
     if(!req.validate(["patchPanelPort"]))return;
     var {patchPanelPort,cableNumber,switchId,switchPort,vlan,device,location,description,location}=req.body;
     try{
@@ -17,7 +17,7 @@ export let newNetNode=async(req,res)=>{
 }
 
 /*          POST /api/netnodes/all            */
-export let allNetNodes=async(req,res)=>{
+export let all_NetNodes=async(req,res)=>{
     try{
         var netNodes= await NetNode.find({},{_id:0}).
         populate({path:"switchId",select:"name"})
@@ -28,26 +28,28 @@ export let allNetNodes=async(req,res)=>{
         var data=[]
         netNodes.map(n=>{
             console.plain("n: ",n)
-            data.push({  
-                location:n.location.name,
+            data.push({   
+                patchPanelPort:n.patchPanelPort,
+                cableNumber:n.cableNumber,
                 switch:n.switchId.name,
                 switchPort:n.switchPort,
-                cableNumber:n.cableNumber,
-                patchPanelPort:n.patchPanelPort,
                 vlan:n.vlan.name,
-                device:n.device.name
+                device:n.device.name,
+                description:n.description,
+                location:n.location.name,
             })
             
         })
 
         var finalResult={columns:{
-            location: "مکان",
+            patchPanelPort: "شماره patch panel",
+            cableNumber: "شماره کابل",
             switch: "سوییچ",
             switchPort: "شماره پورت سوییچ",
-            cableNumber: "شماره کابل",
-            patchPanelPort: "شماره patch panel",
             vlan:"شبکه مجازی",
-            device:"نوع"
+            device:"نوع",
+            description:"توضیحات",
+            location: "مکان",
           },
         netNodesData:data
           }
@@ -57,3 +59,78 @@ export let allNetNodes=async(req,res)=>{
         return res.validSend(500,{error:e});
     }
 }
+
+
+/*          POST /api/netnodes/search            */
+export let search_NetNodes=async(req,res)=>{
+try{
+    var{search}=req.body;
+    if (!search) search = "";
+    var dbQuery={
+        $or:[
+           { "patchPanelPort": 
+           {$regex: search,
+                $options: 'i'}},{
+            "cableNumber": {$regex: search,
+                $options: 'i'},},{
+            // switch: {$regex: search,
+            //     $options: 'i'},
+            "switchPort": {$regex: search,
+                $options: 'i'},},{
+            // vlan:{$regex: search,
+            //     $options: 'i'},
+            // device:{$regex: search,
+            //     $options: 'i'},
+            "description":{$regex: search,
+                $options: 'i'},
+            // location: {$regex: search,
+            //     $options: 'i'},
+            }
+
+        ]
+    }
+    var netNodes= await NetNode.find(dbQuery,{_id:0}).
+        populate({path:"switchId",select:"name"})
+        .populate({path:"vlan",select:"name"})
+        .populate({path:"device",select:"name"})
+        .populate({path:"location",select:"name"});
+        // console.plain(netNodes)
+        var data=[]
+        netNodes.map(n=>{
+            console.plain("n: ",n)
+            data.push({   
+                patchPanelPort:n.patchPanelPort,
+                cableNumber:n.cableNumber,
+                switch:n.switchId.name,
+                switchPort:n.switchPort,
+                vlan:n.vlan.name,
+                device:n.device.name,
+                description:n.description,
+                location:n.location.name,
+            })
+            
+        })
+
+        var finalResult={columns:{
+            patchPanelPort: "شماره patch panel",
+            cableNumber: "شماره کابل",
+            switch: "سوییچ",
+            switchPort: "شماره پورت سوییچ",
+            vlan:"شبکه مجازی",
+            device:"نوع",
+            description:"توضیحات",
+            location: "مکان",
+          },
+        netNodesData:data
+          }
+        return res.validSend(200,{netNodes:finalResult});
+
+}
+catch(e){
+    console.error(e);
+    return res.validSend(500,{error:e});
+}
+}
+  
+  
+  
--- src/app/routes/netNodes.js
+++ src/app/routes/netNodes.js
@@ -6,10 +6,11 @@
 const routes=express.Router();
 
 import Auth from '../middlewares/Auth';
-import {newNetNode,allNetNodes} from '../controllers/netNodeController';
+import {new_NetNode,all_NetNodes,search_NetNodes} from '../controllers/netNodeController';
 
 //ENDPOINTS
-routes.post('/new',Auth,newNetNode);
-routes.post('/all',Auth,allNetNodes);
+routes.post('/new',Auth,new_NetNode);
+routes.post('/all',Auth,all_NetNodes);
+routes.post('/search',Auth,search_NetNodes);
 
 export default routes;
